4d6618f9a814fd9e62b60b4a9f50ac6a062abd2f
co63oc <co63oc@users.noreply.github.com>
2025-06-08 07:48:31 +0800

    FIx

--- test_filter.sh
+++ test_filter.sh
@@ -6,3 +6,5 @@ python engine.py --accuracy=True --api_config_file=tester/api_config/big_and_0si
 echo "paddleonly"
 # python engine.py --accuracy=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_paddleonly.txt --filter "$1"
 python engine.py --paddle_only=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_paddleonly.txt --filter "$1"
+
+# python engine.py --paddle_only=True --api_config_file=report/0size_tensor_gpu/20250319/accuracy/coredump_error/error_config.txt --filter "$1"
