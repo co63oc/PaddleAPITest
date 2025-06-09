d94f79735c32dd641242cd12951d94cd9d4d4b8f
co63oc <co63oc@users.noreply.github.com>
2025-06-09 17:00:57 +0800

    Fix

--- test_filter.sh
+++ test_filter.sh
@@ -1,10 +1,21 @@
 rm -vf ./tester/api_config/test_log/checkpoint.txt
 
 echo $1
-echo "accuracy"
-python engine.py --accuracy=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_accuracy.txt --filter "$1"
-echo "paddleonly"
+# echo "accuracy"
+# python engine.py --accuracy=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_accuracy.txt --filter "$1"
+# echo "paddleonly"
 # python engine.py --accuracy=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_paddleonly.txt --filter "$1"
-python engine.py --paddle_only=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_paddleonly.txt --filter "$1"
+# python engine.py --paddle_only=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_paddleonly.txt --filter "$1"
 
 # python engine.py --paddle_only=True --api_config_file=report/0size_tensor_gpu/20250319/accuracy/coredump_error/error_config.txt --filter "$1"
+
+echo "accuracy"
+python engine.py --accuracy=True --api_config_file=report/0size_tensor_gpu/20250528/accuracy/error_config.txt --filter "$1"
+echo "paddleonly"
+python engine.py --paddle_only=True --api_config_file=report/0size_tensor_gpu/20250528/paddleonly/error_config.txt --filter "$1"
+
+# config_file=tester/api_config/7_0_size/0_size_tensor_1_8_1.txt
+# echo "accuracy"
+# python engine.py --accuracy=True --api_config_file=$config_file --filter "$1"
+# echo "paddleonly"
+# python engine.py --paddle_only=True --api_config_file=$config_file --filter "$1"
