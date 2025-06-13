4a4cefa88aa22cf669a47f9aa3192ba838861272
co63oc <co63oc@users.noreply.github.com>
2025-06-13 08:15:12 +0800

    Fix

--- test_filter_cpu.sh
+++ test_filter_cpu.sh
@@ -1,8 +1,11 @@
 rm -vf ./tester/api_config/test_log/checkpoint.txt
 
 echo $1
-echo "accuracy"
-python engineV2.py --test_cpu True --accuracy=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_accuracy.txt --filter "$1"
-echo "paddleonly"
+#echo "accuracy"
+#python engineV2.py --test_cpu True --accuracy=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_accuracy.txt --filter "$1"
+#echo "paddleonly"
 # python engine.py --accuracy=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_paddleonly.txt --filter "$1"
-python engineV2.py --test_cpu True --paddle_only=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_paddleonly.txt --filter "$1"
+#python engineV2.py --test_cpu True --paddle_only=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_paddleonly.txt --filter "$1"
+
+echo "accuracy"
+python engine.py --accuracy=True --api_config_file=report/0size_tensor_cpu/test_history/20250528/accuracy/error_config.txt --filter "$1"
