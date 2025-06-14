f55eea535cd2e325630563c4bc9dd87a00843032
co63oc <co63oc@users.noreply.github.com>
2025-06-14 16:37:19 +0800

    Fix

--- test_filter_cpu.sh
+++ test_filter_cpu.sh
@@ -8,4 +8,4 @@ echo $1
 #python engineV2.py --test_cpu True --paddle_only=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_paddleonly.txt --filter "$1"
 
 echo "accuracy"
-python engine.py --accuracy=True --api_config_file=report/0size_tensor_cpu/test_history/20250528/accuracy/error_config.txt --filter "$1"
+python engine.py --accuracy=True --test_cpu=True --api_config_file=report/0size_tensor_cpu/test_history/20250528/accuracy/error_config.txt --filter "$1"
