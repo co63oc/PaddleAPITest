76d16126195de087bedcc317b13510269332e10d
co63oc <co63oc@users.noreply.github.com>
2025-06-15 09:16:00 +0800

    Fix

--- test_filter_cpu.sh
+++ test_filter_cpu.sh
@@ -6,6 +6,8 @@ echo $1
 #echo "paddleonly"
 # python engine.py --accuracy=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_paddleonly.txt --filter "$1"
 #python engineV2.py --test_cpu True --paddle_only=True --api_config_file=tester/api_config/big_and_0size/api_config_0sizetensor_paddleonly.txt --filter "$1"
-
+c_file="report/0size_tensor_gpu/error_config.txt"
+c_file="report/0size_tensor_cpu/error_config.txt"
 echo "accuracy"
-python engine.py --accuracy=True --test_cpu=True --api_config_file=report/0size_tensor_cpu/test_history/20250528/accuracy/error_config.txt --filter "$1"
+# python engine.py --accuracy=True --test_cpu=True --api_config_file=report/0size_tensor_cpu/test_history/20250528/accuracy/error_config.txt --filter "$1"
+python engine.py --accuracy=True --test_cpu=True --api_config_file=$c_file --filter "$1"
